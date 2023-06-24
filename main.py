2377e7871113d62c0927afd85b40698ddfc11118
gregoms <gregoryr19@gmail.com>
2023-06-24 16:06:09 -0700

    new final

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
-    original_str = "Python Programming "
+    original_str = "Python Programming"
     sub1 = original_str[0:6]
-    sub2 = original_str[7:19]
+    sub2 = original_str[7:18]
 
-    merge_str = sub2 + sub1
+    merge_str = sub2 + ' ' + sub1
 
     print(sub2)
     print(sub1)
